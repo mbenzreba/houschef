29dd773118705495a13e518f97045e967ac21b28
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-26 14:46:12 -0400

    java works with kotlin

--- android/app/src/main/kotlin/com/example/chef/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chef/MainActivity.kt
@@ -3,4 +3,6 @@ package com.example.chef
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    private var r: Recipe = Recipe()
 }
--- android/app/src/main/kotlin/com/example/chef/Step.kt
+++
@@ -1,8 +0,0 @@
-package com.example.chef
-
-import com.mbenzreba.RecipePatternFinder.RecipeWordTree
-
-class Step(val rwt: RecipeWordTree)
-{
-
-}
