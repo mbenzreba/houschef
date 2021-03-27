60a06b925c5f9daa7d89890c794480f160b6a206
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-27 15:53:20 -0400

    revert changes

--- android/app/src/main/kotlin/com/example/chef/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chef/MainActivity.kt
@@ -3,6 +3,4 @@ package com.example.chef
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-
-    private var r: Recipe = Recipe()
 }
--- android/app/src/main/kotlin/com/example/chef/Recipe.java
+++
@@ -1,5 +0,0 @@
-package com.example.chef;
-
-class Recipe {
-    
-}
