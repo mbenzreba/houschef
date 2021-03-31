8e2d09b7f404de0e4a4a103ab285c58b7e9bdfc4
Mohamed Benzreba <mbenzreba7150@conestogac.on.ca>
2021-03-31 11:57:10 -0400

    configure a methodchannel that is still uncalled

--- android/app/src/main/kotlin/com/example/chef/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chef/MainActivity.kt
@@ -1,6 +1,31 @@
 package com.example.chef
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val ASSISTANT_CHANNEL = "com.example.chef/assistant"
+    
+    // Set up method channel
+    // private val a_channel = MethodChannel(flutterView, ASSISTANT_CHANNEL)
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, ASSISTANT_CHANNEL).setMethodCallHandler {
+            call, result ->
+            // Note: this method is invoked on the main thread.
+            // TODO
+        }
+    }
+
+    private fun startCooking() {
+        // ...
+    }
+
+    private fun tellAssistant() {
+        // ...
+    }
 }
+
