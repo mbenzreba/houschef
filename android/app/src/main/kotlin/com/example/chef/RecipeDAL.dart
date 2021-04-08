aad110fdcd77dbf704eefa1c837ecb7c801efb30
Ethan Hoekstra <Ehoekstra5571@conestogac.on.ca>
2021-04-08 12:03:45 -0400

    Database classes

---
+++ android/app/src/main/kotlin/com/example/chef/RecipeDAL.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class RecipeDataModel {
+
+  String dbName;
+
+
+
+  RecipeDataModel({this.dbName});
+}
---
+++ android/app/src/main/kotlin/com/example/chef/RecipeDataModel.dart
@@ -0,0 +1,10 @@
+class RecipeDataModel {
+  final int name;
+  final String title;
+  final String url;
+  final String imgUrl;
+  final String steps;
+  final String ingredients;
+
+  RecipeDataModel({this.name, this.title, this.url, this.imgUrl, this.steps, this.ingredients});
+}
