bf1096daa06239f44318ce50efa332ddea207e27
Ethan Hoekstra <Ehoekstra5571@conestogac.on.ca>
2021-04-11 21:00:07 -0400

    dal and model comments

--- lib/dal/RecipeDataModel.dart
+++ lib/dal/RecipeDataModel.dart
@@ -1,5 +1,5 @@
-import '';
-
+//CLASS       : RecipeDataModel
+//DESCRIPTION : A datamodel meant to interface with the db by converting to a map
 
 class RecipeDataModel {
   final String title;
@@ -10,7 +10,9 @@ class RecipeDataModel {
 
   RecipeDataModel({this.title, this.url, this.imgUrl, this.steps, this.ingredients});
 
-
+  //METHOD      : toMap
+  //RETURNS     : Map<String, dynamic>
+  //DESCRIPTION : Converts the object to a map and returns it
   Map<String, dynamic> toMap() {
     return {
       'title': title,
